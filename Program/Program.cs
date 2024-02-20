dee4f47f29445c64fbb2ab913221bfd4b912f8dc
PATRIKEEV_89 <patrikeev89@gmail.com>
2024-02-20 22:10:02 +0500

    Add solving task

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите элементы массива через пробел: ");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+        string[] resultArray = FilterStrings(inputArray);
+        Console.WriteLine("Новый массив: " + string.Join(" ", resultArray));
+        Console.ReadLine();
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
